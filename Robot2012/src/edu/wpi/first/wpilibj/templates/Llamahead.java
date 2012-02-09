ce2bbb8213c46607aee17c4e823c591c137223e4
shrewsburyrobotics <shrewsburyrobotics@172.17.27.247>
2012-02-09 17:54:24 -0500

    ADDED ALL THE LLAMAS

---
+++ Robot2012/src/edu/wpi/first/wpilibj/templates/Llamahead.java
@@ -0,0 +1,23 @@
+/*
+ * LlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlama
+ * LlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlama
+ * LlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlamaLlama
+ * 
+ */
+
+
+//MORE LLAMAS
+
+
+package edu.wpi.first.wpilibj.templates;
+
+
+/**
+ * @author Llama
+ */
+public class Llamahead
+{
+    
+    
+    
+}
