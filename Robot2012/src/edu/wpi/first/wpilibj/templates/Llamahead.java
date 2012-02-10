a22c9d45a2d41f5785f52ec7a6af210ff966cbc9
shrewsburyrobotics <shrewsburyrobotics@172.17.18.93>
2012-02-10 18:15:45 -0500

    Created motor control objects.

--- Robot2012/src/edu/wpi/first/wpilibj/templates/Llamahead.java
+++ Robot2012/src/edu/wpi/first/wpilibj/templates/Llamahead.java
@@ -11,14 +11,32 @@
 
 
 package edu.wpi.first.wpilibj.templates;
-
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.can.CANNotInitializedException;
 
 /**
  * @author Llama
  */
 public class Llamahead
 {
+    //Creates class instance
+    private static Llamahead instance;
     
+    //CANJaguar objects
+    public PIDJaguar launchMotor;
+    public Relay pickupMotor;
+    public Relay liftMotor;
+    public Relay loadMotor;
     
-    
+    private Llamahead()
+    {    
+        //Creating motor control objects
+        launchMotor = new PIDJaguar(0, 0, 0, 
+                RobotMap.LLAMAHEAD_LAUNCH_MOTOR_CHANNEL, 
+                RobotMap.LLAMAHEAD_LAUNCH_SPEED_SENSOR_CHANNEL, 
+                RobotMap.LLAMAHEAD_TEETH, 1.0);
+        pickupMotor = new Relay (RobotMap.LLAMAHEAD_PICKUP_MOTOR_CHANNEL);
+        liftMotor = new Relay (RobotMap.LLAMAHEAD_LIFT_MOTOR_CHANNEL);
+        loadMotor = new Relay (RobotMap.LLAMAHEAD_LOAD_MOTOR_CHANNEL);   
+    }
 }
