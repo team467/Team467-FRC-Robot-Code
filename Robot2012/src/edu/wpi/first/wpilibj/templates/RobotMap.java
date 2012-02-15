9b7c844175c2285eecca3c9407c2f727f5f7f07c
aidan <aidan@172.17.24.178>
2012-02-15 17:21:10 -0500

    Merged changes with llamahead code

--- Robot2012/src/edu/wpi/first/wpilibj/templates/RobotMap.java
+++ Robot2012/src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -42,9 +42,9 @@ public class RobotMap
     
     //Llamahead motors
     public static final int LLAMAHEAD_LAUNCH_MOTOR_CHANNEL = 7; //TBD
-    public static final int LLAMAHEAD_SCOOP_MOTOR_CHANNEL = 0; //TBD
+    public static final int LLAMAHEAD_SCOOP_MOTOR_CHANNEL = 1; //TBD
     public static final int LLAMAHEAD_INTAKE_MOTOR_CHANNEL = 0; //TBD
-    public static final int LLAMAHEAD_NECK_MOTOR_CHANNEL = 0; //TBD
+    public static final int LLAMAHEAD_NECK_MOTOR_CHANNEL = 2; //TBD
     
     //Llamahead sensors
     public static final int LLAMAHEAD_LAUNCH_SPEED_SENSOR_CHANNEL = 0; //TBD
