4c90baa3bed018988374bd8edfb84a76675980d6
shrewsburyrobotics <shrewsburyrobotics@172.17.18.93>
2012-02-18 15:09:16 -0500

    Created methods to raise and lower arm as well as to reload the tank with the compressor using a periodic function.

---
+++ Robot2012/src/edu/wpi/first/wpilibj/templates/PneumaticArm.java
@@ -0,0 +1,103 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Solenoid;
+
+/**
+ *
+ * @author shrewsburyrobotics
+ */
+public class PneumaticArm {
+    
+    //Single class instance
+    private static PneumaticArm instance;
+    
+    //Solenoid objects
+    private Solenoid lower;
+    private Solenoid raise;
+    
+    //Compressor object
+    private Relay compressor;
+    
+    //Arm position constants
+    public static final boolean ARM_UP = false;
+    public static final boolean ARM_DOWN = true;
+    
+    //Reloading variables
+    private int ticks = 0;
+    private boolean loaded = true;
+    private static final int RELOAD_TIME = 0; //TBD
+    
+    //Private constructor
+    private PneumaticArm() 
+    {
+        raise = new Solenoid(RobotMap.ARM_RAISE_CHANNEL);
+        lower = new Solenoid(RobotMap.ARM_LOWER_CHANNEL);
+        compressor = new Relay(RobotMap.COMPRESSOR_CHANNEL);
+    }
+    
+    //Returns instance of the class
+    public static PneumaticArm getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new PneumaticArm();
+        }
+        return instance;
+    }
+    
+    /**
+     * Set the position of the arm to either Arm.ARM_UP or Arm.ARM_DOWN
+     * @param position 
+     */
+    public void moveArm(boolean position)
+    {
+        if(position == ARM_UP)
+        {
+            raise.set(true);
+            lower.set(false);
+        }
+        else
+        {
+            raise.set(false);
+            lower.set(true);
+        }
+    }
+    
+    /**
+     * Periodic function to update reloading
+     */
+    public void updateReload()
+    {
+        if (!loaded)
+        {
+            compressor.set(Relay.Value.kOn);
+            raise.set(false);
+            lower.set(false);
+        }
+        else
+        {
+            compressor.set(Relay.Value.kOff);
+        }
+        if (ticks > RELOAD_TIME)
+        {
+            loaded = true;
+        }
+        else
+        {
+            ticks ++;
+        }
+    }
+    
+    /**
+     * Starts compressor reloading
+     */
+    public void reload()
+    {
+        loaded = false;
+    }
+}
