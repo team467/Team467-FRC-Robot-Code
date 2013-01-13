6c71813c768dd4b57687f534e116f8822c2ed97f
Adam <Adam@Adam-Inspiron.shrewsbury.k12.ma.us>
2013-01-12 20:16:26 -0500

    Added NormlizePowerValues

---
+++ WheelSpeedCalibration/src/wheelspeedcalibration/NormalizePowerValues.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wheelspeedcalibration;
+
+/**
+ *
+ * @author Adam
+ */
+public class NormalizePowerValues {
+    
+}
