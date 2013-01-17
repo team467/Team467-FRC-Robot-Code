3b484df07fb174ead8665775561dd5655ec1cc44
Kyle <Kyle@169.254.173.77>
2013-01-17 09:43:33 -0500

    Commented the LeastSquaredRegression function so that it is easier to understand

--- WheelSpeedCalibration/src/wheelspeedcalibration/LeastSquaredRegressionLine.java
+++ WheelSpeedCalibration/src/wheelspeedcalibration/LeastSquaredRegressionLine.java
@@ -18,19 +18,29 @@ public class LeastSquaredRegressionLine
         int sumX = 0;
         int sumY = 0;
        
+        //gets the sum of all the 'x's and 'y's
         for(Point p: arrayList)
         {
             sumX = sumX + (int)(p.power);
             sumY = sumY + (int)(p.speed);
         } 
+        //x vals^2
         int sumXX = sumX * sumX;
+        //y vals^2       
         int sumYY = sumY * sumY;
+        //x vals * y vals
         int sumXY = sumX * sumY;
+        //number of data points
         int numData = arrayList.size();
+        //bar x (average of 'x's)
         int meanX = sumX / numData;
+        //bar y (average of 'y's)
         int meanY = sumY / numData;
+        //'b' val = slope 
         int b = sumXY / sumXX;
+        //'a' val = y intercept
         int a = meanY - b * meanX;
+        //'r' val = correlation coefficent
         double r = Math.sqrt((sumXY * sumXY)/(sumXX * sumYY));
     
     }
