c196540fa3e2d2ad8b4ee973fb230a7eaaf354e0
callan <callan@172.17.18.46>
2014-01-24 21:23:58 -0500

    Unification of steering code with both 2014 robots

--- Robot2014-mule/src/edu/wpi/first/wpilibj/templates/Steering.java
+++ Robot2014-mule/src/edu/wpi/first/wpilibj/templates/Steering.java
@@ -3,21 +3,22 @@ package edu.wpi.first.wpilibj.templates;
 import edu.wpi.first.wpilibj.*;
 
 /**
- * Class to control steering mechanism on Team467 2010 Robot
+ * Class to control steering mechanism on Team467 Robot
  * Uses WPI PID controller
+ *
  * @author shrewsburyrobotics
  */
 public class Steering
 {
     //Sensor used to determine angle
     private AnalogChannel steeringSensor;
-    
+
     //PID Controller object
     private PIDController steeringPID;
-    
+
     //Steering motor
     private Talon steeringMotor;
-    
+
     //Center point of this steering motor
     private double steeringCenter;
 
@@ -31,9 +32,10 @@ public class Steering
             return (steeringSensor.getAverageValue());
         }
     }
-    
+
     /**
      * Constructor for steering subsystem
+     *
      * @param p - P parameter to use in PID
      * @param i - I parameter to use in PID
      * @param d - D parameter to use in PID
@@ -44,19 +46,19 @@ public class Steering
     Steering(double p, double i, double d,
              int motor, int sensor, double center)
     {
-        //Make steering motor
+        // Make steering motor
         steeringMotor = new Talon(motor);        
-        
-        //Make steering sensor
+
+        // Make steering sensor
         steeringSensor = new AnalogChannel(sensor);
 
-        //Set steering center
+        // Set steering center
         steeringCenter = center;
-        
-        //Make PID Controller
+
+        // Make PID Controller
         steeringPID = new PIDController(p, i, d, new SteeringPIDSource(), steeringMotor);
 
-        //Set PID Controller settings
+        // Set PID Controller settings        
         steeringPID.setInputRange(0.0, RobotMap.STEERING_RANGE);
         steeringPID.setSetpoint(steeringCenter);
         steeringPID.setContinuous(true);
@@ -65,36 +67,42 @@ public class Steering
 
     /**
      * Get directly the value of the sensor
-     * @return The sensor value, read from 0 to 990
+     *
+     * @return The sensor value, read from 0 to RobotMap.STEERING_RANGE
      */
     public double getSensorValue()
     {
         return steeringSensor.getAverageValue();
     }
 
+    /**
+     * @return - setpoint of the PID controller
+     */
     public double getSetPoint()
     {
         return steeringPID.getSetpoint();
     }
-    
+
     /**
      * Get the Talon motor of this steering object
-     * @return 
+     * @return
      */
     public Talon getMotor()
     {
         return steeringMotor;
     }
-    
+
     /**
      * Get the sensor angle normalized to a -1.0 to 1.0 range
-     * Implements the steering center point to give an angle accurate to the
+     * Implements the steering center point to give an angle accurate to the 
      * robot's alignment.
-     * @return 
+     *
+     * @return - steering angle
      */
     public double getSteeringAngle()
     {
         double sensor = steeringSensor.getAverageValue() - steeringCenter;
+
         if (sensor < (-RobotMap.STEERING_RANGE / 2))
         {
             sensor += RobotMap.STEERING_RANGE;
@@ -109,7 +117,7 @@ public class Steering
     /**
      * Print steering parameters
      */
-    public void printParameters()
+    public void printSteeringParameters()
     {
         System.out.print("Steering:");
         System.out.print(" P: " + steeringPID.getP());
@@ -122,13 +130,14 @@ public class Steering
     }
 
     /**
-     * Set angle of front steering.  -1.0 = 180 degrees Left, 0.0 = center, 1.0 = 180 degrees right
-     * @param angle
+     * Set angle of front steering. -1.0 = 180 degrees Left, 0.0 = center, 1.0 = 180 degrees right
+     * @param angle - any value between -1 and 1
      */
     public void setAngle(double angle)
     {
         double setPoint;
 
+        // wrap around values to be between 1 and -1
         if (angle < -1.0)
         {
             angle += 2.0;
@@ -138,10 +147,10 @@ public class Steering
             angle -= 2.0;
         }
 
-        //Calculate desired setpoint for PID based on known center position
-        setPoint = steeringCenter + angle * (RobotMap.STEERING_RANGE / 2);
+        // Calculate desired setpoint for PID based on known center position
+        setPoint = steeringCenter + (angle * (RobotMap.STEERING_RANGE / 2));
 
-        //Normalize setPoint into the -990 to +990 range
+        //Normalize setPoint into the 0 to RobotMap.STEERING_RANGE range
         if (setPoint < 0.0)
         {
             setPoint += RobotMap.STEERING_RANGE;
@@ -153,10 +162,10 @@ public class Steering
 
         steeringPID.setSetpoint(setPoint);
     }
-    
+
     /**
      * Change the center point of this steering motor
-     * @param center 
+     * @param center
      */
     public void setCenter(double center)
     {
