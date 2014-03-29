50efb76d678d2699cbe5e1ebd6fe498f08cd63b6
Callan McInally <callan@shrewsburyrobotics.org>
2014-03-29 14:45:49 -0400

    Added autonomous updates made at WPI

--- Robot2014-nonaem/src/edu/wpi/first/wpilibj/templates/Autonomous.java
+++ Robot2014-nonaem/src/edu/wpi/first/wpilibj/templates/Autonomous.java
@@ -16,16 +16,23 @@ public class Autonomous
     private static Camera467 cam;
     private static Launcher launcher;
     private static Feeder feeder;
+    private static Compressor467 compressor;
 
-    private static final int DRIVE_TO_POS_TIME = 1000;       // Drive forward for 1 second
-    private static final int WAIT_TO_SHOOT_TIMEOUT = 8000;   // Shoot after 8 seconds
+    private static final int DRIVE_TO_POS_TIME = 2000;
+    private static final int STABILIZE_TIME = 3000;
+    private static final int DROP_FEEDER_TIME = 3500;
+    private static final int WAIT_TO_SHOOT_TIMEOUT = 6000;
 
     private static final int START = 0;
     private static final int DRIVE_TO_POS = 1;
-    private static final int WAIT_TO_SHOOT = 2;
-    private static final int SHOOT = 3;
+    private static final int WAIT_FOR_STABILIZE = 2;
+    private static final int LOWER_FEEDER = 3;
+    private static final int WAIT_TO_SHOOT = 4;
+    private static final int SHOOT = 5;
 
     private static int state = START;
+
+    private static final boolean USE_CAMERA = false;
 
     static long startTime = 0;
 
@@ -35,10 +42,15 @@ public class Autonomous
     public static void init()
     {
         cam = Camera467.getInstance();
-        cam.startThread();
+        if (USE_CAMERA)
+        {
+            cam.startThread();
+        }
         launcher = Launcher.getInstance();
         feeder = Feeder.getInstance();
+        compressor = Compressor467.getInstance();
         resetState();
+        startTime = 0;
     }
 
     /**
@@ -46,6 +58,7 @@ public class Autonomous
      */
     public static void updateAutonomous()
     {
+        compressor.update();
         long currentTime = System.currentTimeMillis();
         if (startTime == 0)
         {
@@ -54,7 +67,7 @@ public class Autonomous
         long deltaTime = currentTime - startTime;
 
         // make sure camera is reading
-        if (!cam.isReading())
+        if (USE_CAMERA && !cam.isReading())
         {
             cam.toggleReading();
         }
@@ -63,22 +76,39 @@ public class Autonomous
         {
             case START:
                 drive.crabDrive(0, 0, false);
+                feeder.driveFeederMotor(-1.0);//intake ball
                 launcher.pullBackLauncher();
-                feeder.lowerFeeder();
                 state = DRIVE_TO_POS;
                 break;
 
             case DRIVE_TO_POS:
-                drive.crabDrive(0, 0.5, false);
+                drive.crabDrive(0, 0.6, false);                
                 if (deltaTime > DRIVE_TO_POS_TIME)
+                {
+                    state = WAIT_FOR_STABILIZE;
+                }
+                break;
+
+            case WAIT_FOR_STABILIZE:                
+                drive.crabDrive(0, 0, false);
+                if (deltaTime > STABILIZE_TIME)
+                {
+                    state = LOWER_FEEDER;
+                }
+                break;
+                
+            case LOWER_FEEDER:
+                feeder.lowerFeeder();        
+                drive.crabDrive(0, 0, false);
+                if (deltaTime > DROP_FEEDER_TIME)
                 {
                     state = WAIT_TO_SHOOT;
                 }
                 break;
 
             case WAIT_TO_SHOOT:
-                drive.crabDrive(0, 0, false);
-                if (cam.isTargetDetected() || (deltaTime > WAIT_TO_SHOOT_TIMEOUT))
+                drive.crabDrive(0, 0, false);                
+                if ((USE_CAMERA && cam.isTargetDetected()) || (deltaTime > WAIT_TO_SHOOT_TIMEOUT))
                 {
                     state = SHOOT;
                 }
@@ -86,9 +116,10 @@ public class Autonomous
 
             case SHOOT:
                 drive.crabDrive(0, 0, false);
+                feeder.driveFeederMotor(0.0);//stop ball feeder
                 launcher.fireLauncher();
                 break;
-        }       
+        }
     }
 
     /**
